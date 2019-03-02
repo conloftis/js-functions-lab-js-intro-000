e2125ace998157ff6bbb1f1a61391bd82d5f421d
Connor Trent Loftis <cloftis@pm.me>
2019-03-02 15:08:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name){
-  return `Happy holidays, ${name}`
+  return `Happy holidays, ${name}!`
 }
 
-function happyHolidaysTo(holidaym name) {
+function happyHolidaysTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
